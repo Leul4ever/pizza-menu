7cf4fb545e4799e4d7f711f534ca7462a7ae9cdc
Liul-Abera <liulaber43@gmail.com>
2024-09-13 18:08:00 -0400

    final

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React, { StrictMode } from "react"
-import ReactDOM from "react-dom/client"
+import React, { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 
- const pizzaData = [
+const pizzaData = [
   {
     name: "Focaccia",
     ingredients: "Bread with italian olive oil and rosemary",
@@ -47,71 +47,89 @@ import "./index.css";
   },
 ];
 
-function App (){
+function App() {
   return (
     <div className="container ">
-    <Header/>
-    <Menu/>
-    <Footer/>
-
+      <Header />
+      <Menu />
+      <Footer />
     </div>
-  )
-
+  );
 }
-function Header(){
-  const style ={};
-  return <header className="header">
-     <h1 style={style}>Fast React Pizza Company</h1>
-  </header>
+function Header() {
+  const style = {};
+  return (
+    <header className="header">
+      <h1 style={style}>Fast React Pizza Company</h1>
+    </header>
+  );
 }
-function Menu(){
-  return <div>
-    <main className="menu">
-    <h2> Our Menu </h2>
-    <Pizza/>
-      <Pizza/>
-      <Pizza/>
-    </main>
-    
-    </div>
-
-}
-function Footer(){
-  const hour = new Date().getHours();
-  const openHour=9;
-  const closeHour= 22;
-  const isOpen=hour<=openHour && hour>=closeHour
-  console.log(isOpen)
-return (
-  <footer className="footer"> 
-{new Date().toLocaleTimeString()}.We are currently open
-  </footer>
-)
-// return (
-//   React.createElement("footer",null ,"we are currently open ");
-// )
-
-}
-
-function Pizza(){
-  return(
+function Menu() {
+  return (
     <div>
-      <img src="pizzas/spinaci.jpg"></img>
-      <h3>Pizza Spianci</h3>
-      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
+      <main className="menu">
+        <h2> Our Menu </h2>
+        <Pizza
+          name="Pizza Spianci"
+          ingredients="Tomato,mozarella, and ricotta cheese "
+          photoName="Pizzas/spinaci.jpg"
+          price={10}
+        />
+        <Pizza
+          name="Pizza Funghi"
+          ingredients="Tomato, mozarella, mushrooms, and onion"
+          price={12}
+          photoName="pizzas/funghi.jpg"
+        />
+        <Pizza
+          name="Pizza Salamino"
+          ingredients="Tomato, mozarella, and pepperoni"
+          price={15}
+          photoName="pizzas/salamino.jpg"
+          soldOut="true"
+        />
+        <Pizza
+          name="Pizza Prosciutto"
+          ingredients="Tomato, mozarella, ham, aragula, and burrata cheese"
+          price={18}
+          photoName="pizzas/prosciutto.jpg"
+          soldOut="false"
+        />
+      </main>
     </div>
-  )
-
+  );
+}
+function Pizza(props) {
+  console.log(props);
+  return (
+    <div className="pizza ">
+      <img src={props.photoName} />
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>{props.price + 3} </span>
+      </div>
+    </div>
+  );
+}
+function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 9;
+  const closeHour = 22;
+  const isOpen = hour <= openHour && hour >= closeHour;
+  console.log(isOpen);
+  return (
+    <footer className="footer">
+      {new Date().toLocaleTimeString()} .We are currently open
+    </footer>
+  );
+  // return (
+  //   React.createElement("footer",null ,"we are currently open ");
+  // )
 }
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<React.StrictMode><App/></React.StrictMode>);
-
-
-
-
-
-
-
-
-
-
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
